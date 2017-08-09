7243f558773c5cd99d004e63190aa4dcb40c8fdd
洪璽凱 <tpanakin@gmail.com>
2017-08-09 21:49:08 +0800

    add new method, create method to stores controller, create new page

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,4 +2,20 @@ class StoresController < ApplicationController
   def index
     @stores = Store.all
   end
+
+  def new
+    @store = Store.new
+  end
+
+  def create
+    store = Store.new
+
+    store.name = params[:name]
+    store.description = params[:description]
+    store.phone = params[:phone]
+    store.address = params[:address]
+
+    store.save
+    redirect_to '/stores'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get '/stores' => 'stores#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # create new stores
+  get '/stores/new' => 'stores#new'
+  post '/stores' => 'stores#create'
 end
