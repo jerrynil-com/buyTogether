f1ff93fe67b44ec2106e29719a513ca9a41c0baa
洪璽凱 <tpanakin@gmail.com>
2017-08-18 21:44:52 +0800

    add edit and destroy method to items_controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,32 @@
 class ItemsController < ApplicationController
   before_action :set_store
+  before_action :set_item, only:[:edit, :update, :destroy]
+
 
   def create
     if @store.items.create item_params
       redirect_to store_path(@store)
     else
       render 'stores/show'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update item_params
+      redirect_to store_path(@store)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to store_path(@store)
+    else
+      remder 'edit'
     end
   end
 
@@ -17,6 +38,10 @@ class ItemsController < ApplicationController
 
   def set_store
     @store = Store.find(params[:store_id])
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
