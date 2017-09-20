5962dfe0d01b60662a60b954cc9574bf0a96c42d
jerrynil <jerrynil.tw@gmail.com>
2017-09-20 15:03:48 +0800

    add store data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |num|
+  Store.create name: "測試 #{num + 1} 號店",
+        description: "這是第 #{num + 1} 家店",
+        phone: "1234567#{num+1}",
+        address: "羅斯福路 #{num+1} 段"
+end
+
+
+
+
