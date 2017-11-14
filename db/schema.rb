8ae95d094066061904194981e27ce1c1b3d070bd
jerrynil <jerrynil.tw@gmail.com>
2017-11-14 14:49:26 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -22,17 +22,6 @@ ActiveRecord::Schema.define(version: 20170915100010) do
     t.index ["store_id"], name: "index_items_on_store_id"
   end
 
-  create_table "migrations", force: :cascade do |t|
-    t.string "time_period"
-    t.datetime "end_time"
-    t.integer "store_id"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["store_id"], name: "index_migrations_on_store_id"
-    t.index ["user_id"], name: "index_migrations_on_user_id"
-  end
-
   create_table "ordered_items", force: :cascade do |t|
     t.integer "order_id"
     t.integer "item_id"
